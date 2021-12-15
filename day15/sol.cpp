2f2f1a67d205db0d4596d7e1f6268cf1a3ee25bd
M315 <m315@pop-os.localdomain>
2021-12-15 12:11:06 +0100

    Day 15: Dijktra

--- day15/sol.cpp
+++ day15/sol.cpp
@@ -2,161 +2,92 @@
 
 using namespace std;
 
-bool comp(vector<int> a, vector<int> b)
-{
-	return a[0] > b[0];
-}
-
-int is_in_queue(vector<vector<int>> Q, int i, int j)
-{
-	int in = -1;
-	for(int k = 0; k < Q.size(); k++)
-	{
-		if(Q[k][1] == i && Q[k][2] == j)
-		{
-			in = k;
-			break;
-		}
-	}
-	
-	return in;
-}
-
-void path(vector<vector<int>>& map, vector<vector<int>> parent)
-{
-	int n = map.size(), m = map[0].size();
-	
-	int i = n - 1, j = m - 1;
-	
-	int d = 0;
-	
-	while(parent[i][j] != 0)
-	{
-		d += map[i][j];
-		
-		map[i][j] = 0;
-		if(parent[i][j] == 1)
-			i++;
-		else if(parent[i][j] == -1)
-			i--;
-		else if(parent[i][j] == 2)
-			j++;
-		else if(parent[i][j] == -2)
-			j--;
-	}
-	
-	for(i = 0; i < n; i++)
-	{
-		for(j = 0; j < m; j++)
-			cout << map[i][j] << " ";
-		cout << endl;
-	}
-}
-
 int dijktra(vector<vector<int>>& map)
 {
 	int n = map.size(), m = map[0].size();
 	
-	// 0: dist, 1: i, 2: j
-	vector<vector<int>> Q;
-	
-	// -1,1 i-ways, -2,2 j-ways
-	vector<vector<int>> parent = map;
-	
-	for(int i = 0; i < n; i ++)
+	vector<vector<bool>> visited;
+	for(int i = 0; i < n; i++)
 	{
-		for(int j = 0; j < m; j++)
-		{
-			vector<int> vec;
-			
-			// Dist
-			vec.push_back(INT_MAX);
-			
-			// i, j
-			vec.push_back(i);
-			vec.push_back(j);
-			
-			Q.push_back(vec);
-			
-			// parents
-			parent[i][j] = 0;
-		}
+		vector<bool> v (m, false);
+		visited.push_back(v);
 	}
 	
-	// dist source = 0
-	Q[0][0] = 0;
+	// 0: dist, 1: i, 2: j
+	priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>> > Q;
+	
+	vector<int> v (3, 0);
+	Q.push(v);
 	
 	while(Q.size())
 	{
-		// Sort the vector
-		sort(Q.begin(), Q.end(), comp);
+		vector<int> u = Q.top();
+		int d = u[0], i = u[1], j = u[2];
 		
-		vector<int> u = Q.back();
-		int dist = u[0], i = u[1], j = u[2];
+		Q.pop();
 		
-		Q.pop_back();
+		if(visited[i][j])
+			continue;
 		
 		// Check if its de solution
 		if(i == n - 1 && j == m - 1)
 		{
-			path(map, parent);
-			return dist;
+			//path(map, parent);
+			return d;
 		}
+		
+		visited[i][j] = true;
 		
 		// Check neighbours
 		if(i > 0)
 		{
-			int k = is_in_queue(Q, i - 1, j);
-			if(k != -1){
-				int alt = dist + map[i - 1][j];
+			if(!visited[i - 1][j])
+			{
+				vector<int> v (3, 0);
+				v[0] = d + map[i - 1][j];
+				v[1] = i - 1;
+				v[2] = j;
 				
-				if(alt < Q[k][0])
-				{
-					Q[k][0] = alt;
-					parent[i - 1][j] = 1;
-				}
+				Q.push(v);
 			}
 		}
 		
 		if(i < n - 1)
 		{
-			int k = is_in_queue(Q, i + 1, j);
-			if(k != -1){
-				int alt = dist + map[i + 1][j];
+			if(!visited[i + 1][j])
+			{
+				vector<int> v (3, 0);
+				v[0] = d + map[i + 1][j];
+				v[1] = i + 1;
+				v[2] = j;
 				
-				if(alt < Q[k][0])
-				{
-					Q[k][0] = alt;
-					parent[i + 1][j] = -1;
-				}
+				Q.push(v);
 			}
 		}
 		
-		if(j < 0)
+		if(j > 0)
 		{
-			int k = is_in_queue(Q, i, j - 1);
-			if(k != -1){
-				int alt = dist + map[i][j - 1];
+			if(!visited[i][j - 1])
+			{
+				vector<int> v (3, 0);
+				v[0] = d + map[i][j - 1];
+				v[1] = i;
+				v[2] = j - 1;
 				
-				if(alt < Q[k][0])
-				{
-					Q[k][0] = alt;
-					parent[i][j - 1] = 2;
-				}
+				Q.push(v);
 			}
 		}
 		
 		if(j < m - 1)
 		{
-			int k = is_in_queue(Q, i, j + 1);
-			if(k != -1){
-				int alt = dist + map[i][j + 1];
+			if(!visited[i][j + 1])
+			{
+				vector<int> v (3, 0);
+				v[0] = d + map[i][j + 1];
+				v[1] = i;
+				v[2] = j + 1;
 				
-				if(alt < Q[k][0])
-				{
-					Q[k][0] = alt;
-					parent[i][j + 1] = -2;
-				}
+				Q.push(v);
 			}
 		}
 	}
@@ -164,10 +95,312 @@ int dijktra(vector<vector<int>>& map)
 	return 0;
 }
 
+void solve2() 
+{
+	FILE *pf;
+	pf = fopen("input1.txt", "r");
+	
+	vector<vector<int>> map;
+	
+	// Read
+	if(pf != NULL)
+	{
+		char c = '0';
+		
+		while(c != EOF)
+		{
+			vector<int> vec;
+			c = fgetc(pf);
+			
+			while(c != '\n' && c != EOF)
+			{
+				vec.push_back((int)c - 48);
+				c = fgetc(pf);
+			}
+			
+			map.push_back(vec);
+		}
+		
+		map.pop_back();
+	}
+	else{
+		cout << "No such file" << endl;
+	}
+	
+	int n = map.size(), m = map[0].size();
+	
+	// Generate the big map
+	vector<vector<int>> big_map;
+	
+	// Frist lines
+	for(int i = 0; i < n; i++)
+	{
+		vector<int> vec (m * 5);
+		
+		// 1
+		for(int j = 0; j < m; j++)
+			vec[j] = map[i][j];
+		
+		// 2
+		for(int j = 0; j < m; j++)
+		{
+			vec[m + j] = vec[j] + 1;
+			
+			if(vec[m + j] == 10)
+				vec[m + j] = 1;
+		}
+		
+		// 3
+		for(int j = 0; j < m; j++)
+		{
+			vec[(2 * m) + j] = vec[m + j] + 1;
+			
+			if(vec[(2 * m) + j] == 10)
+				vec[(2 * m) + j] = 1;
+		}
+		
+		// 4
+		for(int j = 0; j < m; j++)
+		{
+			vec[(3 * m) + j] = vec[(2 * m) + j] + 1;
+			
+			if(vec[(3 * m) + j] == 10)
+				vec[(3 * m) + j] = 1;
+		}
+		
+		// 5
+		for(int j = 0; j < m; j++)
+		{
+			vec[(4 * m) + j] = vec[(3 * m) + j] + 1;
+			
+			if(vec[(4 * m) + j] == 10)
+				vec[(4 * m) + j] = 1;
+		}
+		
+		big_map.push_back(vec);
+	}
+	
+	// Second lines
+	for(int i = 0; i < n; i++)
+	{
+		vector<int> vec (m * 5);
+		
+		// 1
+		for(int j = 0; j < m; j++)
+		{
+			vec[j] = map[i][j] + 1;
+			
+			if(vec[j] == 10)
+				vec[j] = 1;
+		}
+		
+		// 2
+		for(int j = 0; j < m; j++)
+		{
+			vec[m + j] = vec[j] + 1;
+			
+			if(vec[m + j] == 10)
+				vec[m + j] = 1;
+		}
+		
+		// 3
+		for(int j = 0; j < m; j++)
+		{
+			vec[(2 * m) + j] = vec[m + j] + 1;
+			
+			if(vec[(2 * m) + j] == 10)
+				vec[(2 * m) + j] = 1;
+		}
+		
+		// 4
+		for(int j = 0; j < m; j++)
+		{
+			vec[(3 * m) + j] = vec[(2 * m) + j] + 1;
+			
+			if(vec[(3 * m) + j] == 10)
+				vec[(3 * m) + j] = 1;
+		}
+		
+		// 5
+		for(int j = 0; j < m; j++)
+		{
+			vec[(4 * m) + j] = vec[(3 * m) + j] + 1;
+			
+			if(vec[(4 * m) + j] == 10)
+				vec[(4 * m) + j] = 1;
+		}
+		
+		big_map.push_back(vec);
+	}
+	
+	// Third lines
+	for(int i = 0; i < n; i++)
+	{
+		vector<int> vec (m * 5);
+		
+		// 1
+		for(int j = 0; j < m; j++)
+		{
+			vec[j] = big_map[n + i][j] + 1;
+			
+			if(vec[j] == 10)
+				vec[j] = 1;
+		}
+		
+		// 2
+		for(int j = 0; j < m; j++)
+		{
+			vec[m + j] = vec[j] + 1;
+			
+			if(vec[m + j] == 10)
+				vec[m + j] = 1;
+		}
+		
+		// 3
+		for(int j = 0; j < m; j++)
+		{
+			vec[(2 * m) + j] = vec[m + j] + 1;
+			
+			if(vec[(2 * m) + j] == 10)
+				vec[(2 * m) + j] = 1;
+		}
+		
+		// 4
+		for(int j = 0; j < m; j++)
+		{
+			vec[(3 * m) + j] = vec[(2 * m) + j] + 1;
+			
+			if(vec[(3 * m) + j] == 10)
+				vec[(3 * m) + j] = 1;
+		}
+		
+		// 5
+		for(int j = 0; j < m; j++)
+		{
+			vec[(4 * m) + j] = vec[(3 * m) + j] + 1;
+			
+			if(vec[(4 * m) + j] == 10)
+				vec[(4 * m) + j] = 1;
+		}
+		
+		big_map.push_back(vec);
+	}
+	
+	// Fourth lines
+	for(int i = 0; i < n; i++)
+	{
+		vector<int> vec (m * 5);
+		
+		// 1
+		for(int j = 0; j < m; j++)
+		{
+			vec[j] = big_map[(2 * n) + i][j] + 1;
+			
+			if(vec[j] == 10)
+				vec[j] = 1;
+		}
+		
+		// 2
+		for(int j = 0; j < m; j++)
+		{
+			vec[m + j] = vec[j] + 1;
+			
+			if(vec[m + j] == 10)
+				vec[m + j] = 1;
+		}
+		
+		// 3
+		for(int j = 0; j < m; j++)
+		{
+			vec[(2 * m) + j] = vec[m + j] + 1;
+			
+			if(vec[(2 * m) + j] == 10)
+				vec[(2 * m) + j] = 1;
+		}
+		
+		// 4
+		for(int j = 0; j < m; j++)
+		{
+			vec[(3 * m) + j] = vec[(2 * m) + j] + 1;
+			
+			if(vec[(3 * m) + j] == 10)
+				vec[(3 * m) + j] = 1;
+		}
+		
+		// 5
+		for(int j = 0; j < m; j++)
+		{
+			vec[(4 * m) + j] = vec[(3 * m) + j] + 1;
+			
+			if(vec[(4 * m) + j] == 10)
+				vec[(4 * m) + j] = 1;
+		}
+		
+		big_map.push_back(vec);
+	}
+	
+	// Fifth lines
+	for(int i = 0; i < n; i++)
+	{
+		vector<int> vec (m * 5);
+		
+		// 1
+		for(int j = 0; j < m; j++)
+		{
+			vec[j] = big_map[(3 * n) + i][j] + 1;
+			
+			if(vec[j] == 10)
+				vec[j] = 1;
+		}
+		
+		// 2
+		for(int j = 0; j < m; j++)
+		{
+			vec[m + j] = vec[j] + 1;
+			
+			if(vec[m + j] == 10)
+				vec[m + j] = 1;
+		}
+		
+		// 3
+		for(int j = 0; j < m; j++)
+		{
+			vec[(2 * m) + j] = vec[m + j] + 1;
+			
+			if(vec[(2 * m) + j] == 10)
+				vec[(2 * m) + j] = 1;
+		}
+		
+		// 4
+		for(int j = 0; j < m; j++)
+		{
+			vec[(3 * m) + j] = vec[(2 * m) + j] + 1;
+			
+			if(vec[(3 * m) + j] == 10)
+				vec[(3 * m) + j] = 1;
+		}
+		
+		// 5
+		for(int j = 0; j < m; j++)
+		{
+			vec[(4 * m) + j] = vec[(3 * m) + j] + 1;
+			
+			if(vec[(4 * m) + j] == 10)
+				vec[(4 * m) + j] = 1;
+		}
+		
+		big_map.push_back(vec);
+	}
+	
+	cout << dijktra(big_map) << endl;
+	
+	return;
+}
+
 void solve1() 
 {
 	FILE *pf;
-	pf = fopen("input_test.txt", "r");
+	pf = fopen("input1.txt", "r");
 	
 	vector<vector<int>> map;
 	
@@ -201,9 +434,10 @@ void solve1()
 	return;
 }
 
+
 int main() {
-	solve1();
-	//solve2();
+	//solve1();
+	solve2();
 
 	return 0;
 }
